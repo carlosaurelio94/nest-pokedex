74a5d95de45364845b52046a8202b0bab8fec0d5
Carlos Rodriguez <crodriguez@bacoclub.com.ar>
2025-10-02 16:10:57 -0300

    primer commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    // Para la carpeta public
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+    }),
+
+    // Para la conexión a la base de datos
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+
+    PokemonModule,
+
+    CommonModule
+  ]
+})
+export class AppModule {}
